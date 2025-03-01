4932423410d5125563ad67968c3f98cce16d3d91
seyfu-t <git@seyfu-t.me>
2025-03-01 13:13:18 +0100

    remove prompt for flatpak package install

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -58,15 +58,8 @@ fi
 if ! pkg_installed flatpak; then
     print_log -r "[FLATPAK]" -b "list :: " "flatpak application"
     awk -F '#' '$1 != "" {print "["++count"]", $1}' "${scrDir}/extra/custom_flat.lst"
-    prompt_timer 60 "Install these flatpaks? [Y/n]"
-    fpkopt=${PROMPT_INPUT,,}
-
-    if [ "${fpkopt}" = "y" ]; then
-        print_log -g "[FLATPAK]" -b "install :: " "flatpaks"
-        "${scrDir}/extra/install_fpk.sh"
-    else
-        print_log -y "[FLATPAK]" -b "skip :: " "flatpak installation"
-    fi
+    print_log -g "[FLATPAK]" -b "install :: " "flatpaks"
+    "${scrDir}/extra/install_fpk.sh"
 
 else
     print_log -y "[FLATPAK]" -b " :: " "flatpak is already installed"
